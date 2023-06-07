aa665f0f806052bab66629edc1eedac4ff4e2a10
Vipul Kumar <vipulyaara@gmail.com>
2023-06-07 13:59:51 +0200

    remove unused fields from file response

--- data/models/src/main/java/com/kafka/data/model/item/File.kt
+++ data/models/src/main/java/com/kafka/data/model/item/File.kt
@@ -9,44 +9,20 @@ data class File(
     val album: String? = null,
     @SerialName("artist")
     val artist: String? = null,
-    @SerialName("bitrate")
-    val bitrate: String? = null,
-    @SerialName("btih")
-    val btih: String? = null,
-    @SerialName("crc32")
-    val crc32: String? = null,
     @SerialName("creator")
     val creator: String? = null,
     @SerialName("format")
     val format: String? = null,
     @SerialName("genres")
     val genre: String? = null,
-    @SerialName("height")
-    val height: String? = null,
     @SerialName("length")
     val length: String? = null,
-    @SerialName("md5")
-    val md5: String? = null,
-    @SerialName("mtime")
-    val mtime: String? = null,
     @SerialName("name")
     val name: String,
-    @SerialName("original")
-    val original: String? = null,
-    @SerialName("rotation")
-    val rotation: String? = null,
-    @SerialName("sha1")
-    val sha1: String? = null,
     @SerialName("size")
     val size: String? = null,
-    @SerialName("source")
-    val source: String? = null,
     @SerialName("title")
     val title: String? = null,
-    @SerialName("track")
-    val track: String? = null,
-    @SerialName("width")
-    val width: String? = null
 ) {
     // todo: improve file ids to make them unique
     // this will require a migration to new file ids which might be a breaking change
