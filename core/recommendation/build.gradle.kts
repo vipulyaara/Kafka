730b5919a566defa161810d95b30d0f835868f41
Vipul Kumar <vipulyaara@gmail.com>
2023-07-24 20:43:06 +0200

    Add idea files to vcs

--- core/recommendation/build.gradle.kts
+++ core/recommendation/build.gradle.kts
@@ -5,31 +5,6 @@ plugins {
 
 android {
     namespace = "org.kafka.recommendation"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
