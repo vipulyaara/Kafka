b836b499a7d90de755801dcf2c829e45161f8a3b
Vipul Kumar <vipulyaara@gmail.com>
2023-07-25 15:23:28 +0200

    pre-0.0.19

--- domain/src/main/java/org/kafka/domain/observers/library/ObserveDownloadItem.kt
+++ domain/src/main/java/org/kafka/domain/observers/library/ObserveDownloadItem.kt
@@ -2,7 +2,6 @@ package org.kafka.domain.observers.library
 
 import com.kafka.data.feature.item.ItemWithDownload
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import org.kafka.base.CoroutineDispatchers
@@ -12,11 +11,11 @@ import javax.inject.Inject
 class ObserveDownloadItem @Inject constructor(
     private val dispatchers: CoroutineDispatchers,
     private val observeDownloadedItems: ObserveDownloadedItems,
-) : SubjectInteractor<String, ItemWithDownload>() {
+) : SubjectInteractor<String, ItemWithDownload?>() {
 
-    override fun createObservable(params: String): Flow<ItemWithDownload> {
+    override fun createObservable(params: String): Flow<ItemWithDownload?> {
         return observeDownloadedItems.createObservable(Unit).map { downloadItems ->
             downloadItems.firstOrNull { it.file.fileId == params }
-        }.filterNotNull().flowOn(dispatchers.io)
+        }.flowOn(dispatchers.io)
     }
 }
