567df8ffca113367bd21661595d144337fae3831
vipulkumar <vipulyaara@gmail.com>
2024-05-18 15:43:06 +0530

    Add download button in online reader

--- ui/reader/src/main/java/com/kafka/reader/online/OnlineReader.kt
+++ ui/reader/src/main/java/com/kafka/reader/online/OnlineReader.kt
@@ -10,9 +10,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -31,12 +35,35 @@ import com.google.accompanist.web.WebView as AccompanistWebView
 @Composable
 fun OnlineReader(viewModel: OnlineReaderViewModel = hiltViewModel()) {
     val webViewState by viewModel.webViewState.collectAsStateWithLifecycle()
+    val readerState by viewModel.readerState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+
+    val topAppBarScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    var scrolledY by remember { mutableFloatStateOf(0f) }
+    var isTopAppBarVisible by remember { mutableStateOf(true) }
+
+    fun updateTopAppBarVisibility(dy: Float) {
+        val newScrollY = scrolledY + dy
+        scrolledY = newScrollY
+        isTopAppBarVisible = newScrollY > 0f // Hide when scrolled down
+    }
+
+//    val nestedScrollModifier = Modifier
+//        .nestedScroll(topAppBarScrollBehavior.nestedScrollConnection)
+//        .verticalScroll { dy ->
+//            updateTopAppBarVisibility(dy)
+//        }
 
     Scaffold(
-        modifier = Modifier,
         topBar = {
             if (webViewState != null) {
-                TopBar(webViewState = webViewState!!, viewModel = viewModel)
+                TopBar(
+                    webViewState = webViewState!!,
+                    isDownloadAvailable = readerState?.fileId != null,
+                    downloadItem = { viewModel.downloadItem(context) },
+                    shareItem = { viewModel.shareItem(context) },
+                    goBack = viewModel::goBack
+                )
             }
         }
     ) { paddingValues ->
@@ -69,16 +96,23 @@ private fun WebView(
 }
 
 @Composable
-private fun TopBar(webViewState: WebViewState, viewModel: OnlineReaderViewModel) {
-    val context = LocalContext.current
-
+private fun TopBar(
+    webViewState: WebViewState,
+    isDownloadAvailable: Boolean,
+    downloadItem: () -> Unit,
+    shareItem: () -> Unit,
+    goBack: () -> Unit
+) {
     Column {
         TopBar(
             title = webViewState.pageTitle.orEmpty(),
-            navigationIcon = { CloseButton(onClick = viewModel::goBack) },
+            navigationIcon = { CloseButton(onClick = goBack) },
             actions = {
+                if (isDownloadAvailable) {
+                    DownloadIcon(onClick = downloadItem)
+                }
                 if (webViewState.lastLoadedUrl != null) {
-                    ShareUrlIcon { viewModel.shareItem(context = context) }
+                    ShareUrlIcon(onClick = shareItem)
                 }
             }
         )
@@ -87,6 +121,15 @@ private fun TopBar(webViewState: WebViewState, viewModel: OnlineReaderViewModel)
             LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
         }
     }
+}
+
+@Composable
+private fun DownloadIcon(onClick: () -> Unit) {
+    IconButton(
+        imageVector = Icons.Download,
+        onClick = onClick,
+        tint = MaterialTheme.colorScheme.primary
+    )
 }
 
 @Composable
