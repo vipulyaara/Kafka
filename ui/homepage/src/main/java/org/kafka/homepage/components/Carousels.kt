51150ea71444b6ecb3ab02290543f8b6cf657ee9
vipulkumar <vipulyaara@gmail.com>
2024-08-22 02:49:27 +0530

    Indicator for carousels

--- ui/homepage/src/main/java/org/kafka/homepage/components/Carousels.kt
+++ ui/homepage/src/main/java/org/kafka/homepage/components/Carousels.kt
@@ -1,5 +1,6 @@
 package org.kafka.homepage.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListState
@@ -7,10 +8,12 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.kafka.data.entities.Item
 import kotlinx.collections.immutable.ImmutableList
 import org.kafka.ui.components.item.FeaturedItem
+import org.kafka.ui.components.material.CircleIndicator
 import ui.common.theme.theme.Dimens
 
 @Composable
@@ -23,21 +26,26 @@ internal fun Carousels(
 ) {
     val state = rememberPagerState { carouselItems.size }
 
-    HorizontalPager(
-        state = state,
-        modifier = modifier
-            .padding(Dimens.Spacing02)
-            .padding(top = Dimens.Spacing12, bottom = Dimens.Spacing08),
-        contentPadding = PaddingValues(horizontal = Dimens.Spacing24),
-        pageSpacing = Dimens.Spacing04,
-    ) { index ->
-        carouselItems.getOrNull(index)?.let { item ->
-            FeaturedItem(
-                item = item,
-                label = item.title,
-                imageUrl = images.getOrNull(index),
-                onClick = { onBannerClick(item.itemId) }
-            )
+    Column(
+        modifier = modifier.padding(top = Dimens.Spacing12),
+        horizontalAlignment = Alignment.End
+    ) {
+        HorizontalPager(
+            state = state,
+            modifier = modifier.padding(Dimens.Spacing02),
+            contentPadding = PaddingValues(horizontal = Dimens.Spacing24),
+            pageSpacing = Dimens.Spacing04,
+        ) { index ->
+            carouselItems.getOrNull(index)?.let { item ->
+                FeaturedItem(
+                    item = item,
+                    label = item.title,
+                    imageUrl = images.getOrNull(index),
+                    onClick = { onBannerClick(item.itemId) }
+                )
+            }
         }
+
+        CircleIndicator(state = state, modifier = Modifier.padding(horizontal = Dimens.Gutter))
     }
 }
