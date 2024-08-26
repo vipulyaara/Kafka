66283eb5d01ffd0b837c65352ea8dd2c11bace00
vipulkumar <vipulyaara@gmail.com>
2024-08-26 13:56:26 +0530

    cleanup

--- ui/homepage/src/main/java/org/kafka/homepage/components/Carousels.kt
+++ ui/homepage/src/main/java/org/kafka/homepage/components/Carousels.kt
@@ -2,8 +2,6 @@ package org.kafka.homepage.components
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
 import androidx.compose.material3.carousel.rememberCarouselState
@@ -11,18 +9,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.kafka.data.entities.Item
-import kotlinx.collections.immutable.ImmutableList
 import org.kafka.ui.components.item.FeaturedItem
 import ui.common.theme.theme.Dimens
 
 @Composable
 internal fun Carousels(
-    carouselItems: ImmutableList<Item>,
+    carouselItems: List<Item>,
     images: List<String>,
     onBannerClick: (String) -> Unit,
     modifier: Modifier = Modifier,
     showLabel: Boolean = true,
-    lazyListState: LazyListState = rememberLazyListState(),
 ) {
     val state = rememberCarouselState { carouselItems.size }
     HorizontalMultiBrowseCarousel(
