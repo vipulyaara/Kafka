13a5cfb0c986d8052107abf9d57c4b0e1f0df1be
vipulkumar <vipulyaara@gmail.com>
2024-08-30 17:29:38 +0530

    Revert Carousel - Material3 Carousel has UI issues with recompositions;

--- ui/homepage/src/main/java/org/kafka/homepage/components/Carousels.kt
+++ ui/homepage/src/main/java/org/kafka/homepage/components/Carousels.kt
@@ -1,11 +1,15 @@
 package org.kafka.homepage.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
 import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.kafka.data.entities.Item
@@ -14,6 +18,38 @@ import ui.common.theme.theme.Dimens
 
 @Composable
 internal fun Carousels(
+    carouselItems: List<Item>,
+    images: List<String>,
+    onBannerClick: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    showLabel: Boolean = true,
+) {
+    val state = rememberPagerState { carouselItems.size }
+
+    Column(
+        modifier = modifier.padding(top = Dimens.Spacing12),
+        horizontalAlignment = Alignment.End
+    ) {
+        HorizontalPager(
+            state = state,
+            modifier = Modifier.padding(Dimens.Spacing02),
+            contentPadding = PaddingValues(horizontal = Dimens.Spacing24),
+            pageSpacing = Dimens.Spacing04,
+        ) { index ->
+            carouselItems.getOrNull(index)?.let { item ->
+                FeaturedItem(
+                    item = item,
+                    label = item.title.takeIf { showLabel },
+                    imageUrl = images.getOrNull(index),
+                    onClick = { onBannerClick(item.itemId) }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+internal fun CarouselsMaterial3(
     carouselItems: List<Item>,
     images: List<String>,
     onBannerClick: (String) -> Unit,
